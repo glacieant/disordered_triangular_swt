01a40b5905bd039afd603829eba0d081013ef632
Santanu Dey <in.santanu@outlook.com>
2017-09-08 13:40:04 +0200

    updated with bond impurity calculation

---
+++ prog/bond_impurity/src/calc_mfparam.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+### This is the subroutine to calculate the various mean ###
+### field parameters from the eigenvectors of the mean   ###
+### hamiltonian
+
+import numpy as np
+import tool
+
+
+def paramset(NSYS,INUM,nbr,XPAR,
+        UPAR,T,J,eigen,vec,
+        bond,chi,
+        M,B,
+        fnum,lmult):
+
+    # the fermi function array for temperature T
+    farray = tool.fermi(eigen,T)
+
+    # iteration over the lattice sites
+    for i in range(0,NSYS):
+
+        # getting the flavour number
+        fnum[i] = (np.vdot(vec[i,:],
+            farray*vec[i,:]) + 
+            np.vdot(vec[i+NSYS,:],
+                farray*vec[i+NSYS,:]) - 1.0).real
+
+        # setting up the lagrange multiplier
+        lmult[i] += fnum[i]/20.0
+
+        # the flavour variable matrix
+        ada = np.vdot(vec[i,:],farray*vec[i,:])
+        adb = np.vdot(vec[i,:],farray*vec[i+NSYS,:])
+        bda = adb.conj()
+        bdb = np.vdot(vec[i+NSYS,:],farray*vec[i+NSYS,:])
+        
+        # getting the magnetic moments
+        MAG = np.array([adb+bda,
+            (-adb+bda)*1.0j,ada-bdb]).real
+        rx = np.random_intel.uniform(UPAR[0][0],
+                UPAR[0][1])
+        M[1,i] = (1.0-rx)*M[1,i]+ rx*MAG
+
+    for i in range(0,NSYS):
+
+        # initiating Zero magnetic fields
+        BMAG = np.array([0.0,0.0,0.0])
+        # iteration over the neighbours
+        for j in nbr[i]:
+
+            # computing the bond operators
+            if j > i:
+
+                xbond = (np.vdot(vec[i,:],farray*vec[j,:])
+                        +np.vdot(vec[i+NSYS,:],
+                            farray*vec[j+NSYS,:]))
+                tx = np.random_intel.uniform(UPAR[1][0],
+                        UPAR[1][1])
+                bond[1,i,j] = (1.0-tx)*bond[1,i,j] + tx*xbond
+                bond[1,j,i] = bond[1,i,j].conj()
+                # updating hopping matrix
+                chi[i,j] = 0.5*(1.0-XPAR)*J[i,j]*bond[1,i,j]
+                chi[j,i] = chi[i,j].conj()
+
+            # computing the magnetic fields
+            BMAG += 0.5*XPAR*J[i,j]*M[1,j]
+
+        # Updating B fields
+        B[i] = BMAG
+
+
---
+++ prog/bond_impurity/src/tool.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+### This is an assortment of programs, ###
+### useful for the main computation    ###
+
+import numpy as np
+
+# the finite temperature fermi function
+def fermi(E,T):
+
+    # with a factor of 2 for degenracy
+    return 1.0/(np.exp(E/T)+1.0)
+
+def init_cpl(NSYS,nbr,
+        ZCO,XPAR,
+        DELTA,ALPHA,J):
+
+    # zeroing out the couplings
+
+    J[:] = 0.0
+
+    if DELTA != 0.0:
+        JBANK = np.random.lognormal(1.0,DELTA,size=(NSYS*12))
+    else:
+        JBANK = np.full((NSYS*12),1.0)
+
+    q = 0
+    for i in range(0,NSYS):
+        for m in range(0,6):
+            j = nbr[i,m]
+            if j > i:
+                J[i][j] = JBANK[q]
+                J[j][i] = J[i][j]
+                q += 1
+        for m in range(6,12):
+            j = nbr[i,m]
+            if j > i:
+                J[i][j] = ALPHA*JBANK[q]
+                J[j][i] = J[i][j]
+                q += 1
+
+
+def init_param(NSYS,nbr,
+        ZCO,XPAR,
+        DELTA,ALPHA,
+        ANGVAR,BONDVAR,
+        J,bond,chi,
+        M,B,fnum,lmult):
+    
+    # zeroing out the parameter matrices
+    
+    chi[:] = 0.0
+    bond[:] = 0.0
+    M[:] = 0.0
+    B[:] = 0.0
+    lmult[:] = 0.0
+    fnum[:] = 0.0
+
+    # setting up the bond matrices
+    for i in range(0,NSYS):
+
+        for j in nbr[i]:
+
+            if j > i:
+
+                bond[1,i,j] = (np.random.uniform(-BONDVAR,BONDVAR)
+                        + np.random.uniform(-BONDVAR,BONDVAR)*1.0j)
+                bond[1,j,i] = bond[1,i,j].conj()
+                chi[i,j] = 0.5*(1.0-XPAR)*J[i,j]*bond[1,i,j]
+                chi[j,i] = chi[i,j].conj()
+    
+    M[1] = np.random.uniform(-ANGVAR,ANGVAR,size=(NSYS,3))
+    
+    for i in range(0,NSYS):
+        
+        for j in nbr[i]:
+            
+            B[i] += 0.5*XPAR*J[i,j]*M[1,j]
+
+
